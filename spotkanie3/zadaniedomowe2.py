c7a4c5b4e88f955c06e4da55c35d1b84dc6bb3d2
Daniel Duchnicki <duchnicki.daniel@gmail.com>
2018-12-09 19:09:17 +0100

    Dodanie zadań domowy nr 2 i 3 ze spotkania 3.

---
+++ spotkanie3/zadaniedomowe2.py
@@ -0,0 +1,45 @@
+from urllib.request import *
+from collections import defaultdict
+import re
+
+def statystyka(plik):
+    slownik = defaultdict(int)
+    plik_do_czytania = urlopen(plik)
+
+    for linia in plik_do_czytania:
+        linia_zdekodowana = linia.decode("utf-8").lower()
+        for slowo in re.split(' |!|\\?|\.|;|:|, |,|\*|\n|/|—|\r', linia_zdekodowana):
+            if not re.match(r'^\s*$', slowo):
+                slownik[slowo] +=1
+
+    slownik_posortowany_po_wartosciach = {}
+    for slowo in sorted(slownik, key=slownik.get, reverse=True):
+        slownik_posortowany_po_wartosciach[slowo] = slownik[slowo]
+
+    return slownik_posortowany_po_wartosciach
+
+
+def porownaj_statystyki(statystyka1, statystyka2):
+    for klucz_stat1 in statystyka1:
+        for klucz_stat2 in statystyka2:
+            if klucz_stat1 == klucz_stat2:
+                print("Słowo '{0}' występuje w obu tekstach. Różnica: {1}".
+                      format(klucz_stat1, abs(statystyka1[klucz_stat1]-statystyka2[klucz_stat2])))
+
+    for klucz_stat1 in statystyka1:
+        if klucz_stat1 not in statystyka2:
+            print("Słowo '{0}' występuje tylko w tekście nr 1.".format(klucz_stat1))
+
+    for klucz_stat2 in statystyka2:
+        if klucz_stat2 not in statystyka1:
+            print("Słowo '{0}' występuje tylko w tekście nr 2.".format(klucz_stat2))
+
+
+makbet = "https://wolnelektury.pl/media/book/txt/makbet.txt"
+wiele_halasu_o_nic = "https://wolnelektury.pl/media/book/txt/shakespeare-wiele-halasu-o-nic.txt"
+
+slowa_makbet = statystyka(makbet)
+slowa_wiele_halasu_o_nic = statystyka(wiele_halasu_o_nic)
+print(slowa_makbet)
+print(slowa_wiele_halasu_o_nic)
+porownaj_statystyki(slowa_makbet, slowa_wiele_halasu_o_nic)
---
+++ spotkanie3/zadaniedomowe3.py
@@ -0,0 +1,41 @@
+def wczytaj_plik_notowan(plik_notowan):
+    notowania = {}
+    with open(plik_notowan, 'r') as plik:
+        for line in plik:
+            spolka = line.split(',')
+            notowania[spolka[0]] = spolka[1]
+
+    return notowania
+
+
+def policz_wartosc_spolek(akcje, notowania):
+    twoj_portfel = {}
+    for spolka in akcje:
+        if spolka in notowania:
+            twoj_portfel[spolka] = (akcje[spolka], float(akcje[spolka]) * float(notowania[spolka]))
+
+    return twoj_portfel
+
+
+def policz_wartosc_portfela(twoj_portfel):
+    wartosc_calego_portfela = 0
+    for spolka in twoj_portfel:
+        wartosc_calego_portfela += twoj_portfel[spolka][1]
+
+    return wartosc_calego_portfela
+
+
+def wyswietl_portfel(twoj_portfel):
+    wartosc_portfela = policz_wartosc_portfela(twoj_portfel)
+    print("Twoje spółki:\n")
+    for spolka in twoj_portfel:
+        print("{0}: ilość akcji: {1}, wartość: {2} zł".format(spolka, twoj_portfel[spolka][0], twoj_portfel[spolka][1]))
+    print("\nWartość całego portfela: {0} zł".format(wartosc_portfela))
+
+
+portfel = {"Energa SA": 500, "PGE SA": 400, "PGNiG SA": 1000, "Tauron Polska Energia SA": 1000,
+           "Trakcja PRKiI SA": 1000, "Ursus SA": 1000}
+plik_z_notowaniami = 'moneypl-1544365631032.csv'
+
+portfel_z_wartosciami = policz_wartosc_spolek(portfel, wczytaj_plik_notowan(plik_z_notowaniami))
+wyswietl_portfel(portfel_z_wartosciami)
